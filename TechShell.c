44c658b4acebac232aab8eaeff9af603634493ee
Callum <CallumBNeal@gmail.com>
2026-02-19 14:04:20 -0600

    Final Changes

--- TechShell.c
+++ TechShell.c
@@ -1,5 +1,5 @@
-// Name(s):
-// Description:
+// Name(s): Callum Neal, Madison Slayden, Jack Tomlin
+// Description: BASH clone in C
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
@@ -32,7 +32,7 @@ void displayPrompt()
 void getInput(char *input, size_t size)
 {
     fgets(input, size, stdin);
-    input[strcspn(input, "\n")] = '\0';  // remove newline
+    input[strcspn(input, "\n")] = '\0'; 
 }
 
 
@@ -83,25 +83,6 @@ struct ShellCommand parseInput(char *input)
 
 
 		
-/*
-    A function that parses through the user input.
-    Consider having this function return a struct that stores vital
-    information about the parsed instruction such as:
-    - The command itself
-    - The arguments that come after the command 
-        Hint: When formatting your data, 
-        look into execvp and how it takes in args.
-    - Information about if a redirect was detected such as >, <, or |
-    - Information about whether or not a new file 
-        needs to be created and what that filename may be.
-    
-
-    Some helpful functions when doing this come from string.h and stdlib.h, such as
-    strtok, strcmp, strcpy, calloc, malloc, realloc, free, and more
-
-    Be sure to consider/test for situations when a backslash is used to escape the space char
-    and when quotes are used to group together various tokens.
-*/
 void executeCommand(struct ShellCommand command)
 {
     if (command.command == NULL)
@@ -173,26 +154,6 @@ void executeCommand(struct ShellCommand command)
         wait(NULL);
     }
 }
-/*
-    A function that executes the command. 
-    This function might take in a struct that represents the shell command.
-
-    Be sure to consider each of the following:
-    1. The execvp() function. 
-        This can execute commands that already exist, that is, 
-        you don't need to recreate the functionality of 
-        the commands on your computer, just the shell.
-        Keep in mind that execvp takes over the current process.
-    2. The fork() function. 
-        This can create a process for execvp to take over.
-    3. cd is not a command like ls and mkdir. 
-        cd is a toold provided by the shell, 
-        so you WILL need to recreate the functionality of cd.
-    4. Be sure to handle standard output redirect and standard input redirects here 
-        That is, there symbols: > and <. 
-        Pipe isn't required but could be a nice addition.
-*/
-
 
 int main() // MAIN
 {
